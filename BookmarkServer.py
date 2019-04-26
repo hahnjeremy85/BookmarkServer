eac1bea4d3e533fdef82c595efa909531d4f280a
Jeremy Hahn <hahnjeremy85@gmail.com>
2019-04-25 20:52:29 -0400

    Correct indent error.

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -1,49 +1,9 @@
 #!/usr/bin/env python3
 #
-# A *bookmark server* or URI shortener that maintains a mapping (dictionary)
-# between short names and long URIs, checking that each new URI added to the
-# mapping actually works (i.e. returns a 200 OK).
-#
-# This server is intended to serve three kinds of requests:
-#
-#   * A GET request to the / (root) path.  The server returns a form allowing
-#     the user to submit a new name/URI pairing.  The form also includes a
-#     listing of all the known pairings.
-#   * A POST request containing "longuri" and "shortname" fields.  The server
-#     checks that the URI is valid (by requesting it), and if so, stores the
-#     mapping from shortname to longuri in its dictionary.  The server then
-#     redirects back to the root path.
-#   * A GET request whose path contains a short name.  The server looks up
-#     that short name in its dictionary and redirects to the corresponding
-#     long URI.
-#
-# Your job in this exercise is to finish the server code.
-#
-# Here are the steps you need to complete:
-#
-# 1. Write the CheckURI function, which takes a URI and returns True if a
-#    request to that URI returns a 200 OK, and False otherwise.
-#
-# 2. Write the code inside do_GET that sends a 303 redirect to a known name.
-#
-# 3. Write the code inside do_POST that sends a 400 error if the form fields
-#    are missing.
-#
-# 4. Write the code inside do_POST that sends a 303 redirect to the form
-#    after saving a newly submitted URI.
-#
-# 5. Write the code inside do_POST that sends a 404 error if a URI is not
-#    successfully checked (i.e. if CheckURI returns false).
-#
-# In each step, you'll need to delete a line of code that raises the
-# NotImplementedError exception.  These are there as placeholders in the
-# starter code.
-#
-# After writing each step, restart the server and run test.py to test it.
-
+# A *bookmark server* or URI shortener.
+import os
 import http.server
 import requests
-import os
 from urllib.parse import unquote, parse_qs
 
 memory = {}
@@ -69,7 +29,7 @@ form = '''<!DOCTYPE html>
 
 
 def CheckURI(uri, timeout=5):
-     '''Check whether this URI is reachable, i.e. does it return a 200 OK?
+    '''Check whether this URI is reachable, i.e. does it return a 200 OK?
 
     This function returns True if a GET request to uri returns a 200 OK, and
     False if that GET request returns any other response, or doesn't return
@@ -92,6 +52,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 
         if name:
             if name in memory:
+                # We know that name! Send a redirect to it.
                 self.send_response(303)
                 self.send_header('Location', memory[name])
                 self.end_headers()
@@ -132,14 +93,12 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             # This URI is good!  Remember it under the specified name.
             memory[shortname] = longuri
 
-            # 4. Serve a redirect to the root page (the form).
+            # Serve a redirect to the form.
             self.send_response(303)
             self.send_header('Location', '/')
             self.end_headers()
         else:
             # Didn't successfully fetch the long URI.
-
-            # 5. Send a 404 error with a useful message.
             self.send_response(404)
             self.send_header('Content-type', 'text/plain; charset=utf-8')
             self.end_headers()
